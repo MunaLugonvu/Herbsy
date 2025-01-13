54c0bc8cdface7a50308778396c0a291600325e0
Muna Lugonvu <munalugonvu@gmail.com>
2025-01-13 15:52:00 +0300

    Add Sign In Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header/header.jsx';
 import Footer from './components/Footer/footer.jsx';
-
+import SignIn from './Auth/signIn.jsx';
 
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto p-4">
-          <Routes>
-            <Route path="/" element={<h2>Welcome to Herbsy</h2>} />
-            <Route path="/products" element={<h2>Products Page</h2>} />
-            <Route path="/about" element={<h2>About Herbsy</h2>} />
-            <Route path="/signin" element={<h2>Sign In Page</h2>} />
-            <Route path="/signout" element={<h2>Sign Out Page</h2>} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<h2>Welcome to Herbsy</h2>} />
+          <Route path="/products" element={<h2>Products Page</h2>} />
+          <Route path="/about" element={<h2>About Herbsy</h2>} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signout" element={<h2>Sign Out Page</h2>} />
+        </Routes>
+      </Layout>
     </Router>
+  );
+};
+
+const Layout = ({ children }) => {
+  const location = useLocation();
+
+  // Define routes that should NOT have a header or footer
+  const noHeaderFooterRoutes = ['/signin', '/signout'];
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {!noHeaderFooterRoutes.includes(location.pathname) && <Header />}
+      <main className="flex-grow container mx-auto p-4">
+        {children}
+      </main>
+      {!noHeaderFooterRoutes.includes(location.pathname) && <Footer />}
+    </div>
   );
 };
 
