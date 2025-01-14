5470042eb4acaba5c5f10a18d7547f01037842ac
Muna Lugonvu <munalugonvu@gmail.com>
2025-01-14 15:00:00 +0300

    Edit Header

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
           <div className="flex items-center lg:order-3">
             <Link
               to="/signin"
-              className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+              className="text-gray-700 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg  px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
             >
               Sign In
             </Link>
