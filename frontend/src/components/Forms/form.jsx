09bd9673897f7bb8a1a7cd43aa42bf740f1284a1
Muna Lugonvu <munalugonvu@gmail.com>
2025-01-16 14:17:34 +0300

    fix form links

--- frontend/src/components/Forms/form.jsx
+++ frontend/src/components/Forms/form.jsx
@@ -1,105 +1,110 @@
 import React, { useState } from "react";
-import logo from '../../images/logo.svg';
+import logo from "../../images/logo.svg";
+import { Link } from "react-router-dom";
 
-const Form = ({ title, fields, onSubmit, buttonText, footerText, footerLink, footerLinkText }) => {
-    const [formData, setFormData] = useState(
-        fields.reduce((acc, field) => ({ ...acc, [field.name]: "" }), {})
-    );
+const Form = ({
+  title,
+  fields,
+  onSubmit,
+  buttonText,
+  footerText,
+  footerLink,
+  footerLinkText,
+}) => {
+  const [formData, setFormData] = useState(
+    fields.reduce((acc, field) => ({ ...acc, [field.name]: "" }), {})
+  );
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSubmit(formData);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(formData);
+  };
 
-    return (
-        <section className="bg-gray-50 dark:bg-gray-900">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a
-                    href="/"
-                    className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-                >
-                    <img
-                        className="w-8 h-8 mr-2"
-                        src={logo}
-                        alt="logo"
-                    />
-                    Herbsy
-                </a>
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            {title}
-                        </h1>
-                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-                            {fields.map((field) => (
-                                <div key={field.name}>
-                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                        {field.label}
-                                    </label>
-                                    <input
-                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-green-600 sm:text-sm/6"
-                                        type={field.type}
-                                        name={field.name}
-                                        value={formData[field.name]}
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                            ))}
-                            {buttonText === "Sign In" && (
-                                <div className="flex items-center justify-between">
-                                    <div className="flex items-start">
-                                        <div className="flex items-center h-5">
-                                            <input
-                                                id="remember"
-                                                aria-describedby="remember"
-                                                type="checkbox"
-                                                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-green-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-green-600 dark:ring-offset-gray-800"
-                                                required=""
-                                            />
-                                        </div>
-                                        <div className="ml-3 text-sm">
-                                            <label
-                                                htmlFor="remember"
-                                                className="text-gray-500 dark:text-gray-300"
-                                            >
-                                                Remember me
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <a
-                                        href="/"
-                                        className="text-sm font-medium text-green-600 hover:underline dark:text-green-500"
-                                    >
-                                        Forgot password?
-                                    </a>
-                                </div>
-                            )}
-                            <button
-                                type="submit"
-                                className="w-full text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                            >
-                                {buttonText}
-                            </button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                {footerText}{" "}
-                                <a
-                                    href={footerLink}
-                                    className="font-medium text-green-600 hover:underline dark:text-green-500"
-                                >
-                                    {footerLinkText}
-                                </a>
-                            </p>
-                        </form>
-                    </div>
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Link
+          to="/"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <img className="w-8 h-8 mr-2" src={logo} alt="logo" />
+          Herbsy
+        </Link>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              {title}
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              {fields.map((field) => (
+                <div key={field.name}>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    {field.label}
+                  </label>
+                  <input
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-green-600 sm:text-sm/6"
+                    type={field.type}
+                    name={field.name}
+                    value={formData[field.name]}
+                    onChange={handleChange}
+                  />
                 </div>
-            </div>
-        </section>
-    );
+              ))}
+              {buttonText === "Sign In" && (
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-green-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-green-600 dark:ring-offset-gray-800"
+                        required=""
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label
+                        htmlFor="remember"
+                        className="text-gray-500 dark:text-gray-300"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
+                  <Link
+                    to="/"
+                    className="text-sm font-medium text-green-600 hover:underline dark:text-green-500"
+                  >
+                    Forgot password?
+                  </Link>
+                </div>
+              )}
+              <button
+                type="submit"
+                className="w-full text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              >
+                {buttonText}
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                {footerText}{" "}
+                <Link
+                  to="/signup"
+                  className="font-medium text-green-600 hover:underline dark:text-green-500"
+                >
+                  {footerLinkText}
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Form;
