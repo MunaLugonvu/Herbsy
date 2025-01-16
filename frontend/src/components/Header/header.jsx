3ff01b13d98fa6fa9f4c606c6094f50a6520f6c6
Muna Lugonvu <munalugonvu@gmail.com>
2025-01-16 13:49:33 +0300

    edit header shadow

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -5,7 +5,7 @@ import logo from "../../images/logo.svg";
 const Header = () => {
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 shadow-lg">
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 shadow-md">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <a
             href="/"
