68c4a2703d5e0c734cbfa6aff8d17119d16bebcf
Muna Lugonvu <munalugonvu@gmail.com>
2025-01-16 13:49:50 +0300

    Add featured articles on home page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import FeaturedProducts from '../components/Products/featuredProducts';
+import FeaturedArticles from '../components/Articles/featuredArticles';
 
 const Home = () => {
   return (
     <>
-    <section className="bg-white dark:bg-gray-900">
-    <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <section className="bg-white dark:bg-gray-900  border-b-2">
+    <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 ">
         <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Welcome to Herbsy</h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">  
@@ -26,6 +27,7 @@ const Home = () => {
     </div>
 </section>
 <FeaturedProducts />
+<FeaturedArticles/>
     </>
   );
 };
