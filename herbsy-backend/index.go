302d1b7c94afa25065ee57f8f6584e9245d03580
Muna Lugonvu <munalugonvu@gmail.com>
2026-02-06 17:12:55 +0300

    Update dependencies in go.mod and go.sum; add CORS middleware in index.go

--- herbsy-backend/index.go
+++ herbsy-backend/index.go
@@ -3,13 +3,14 @@ package main
 import (
 	"net/http"
 	"os"
-
+  "github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Create a Gin router with default middleware (logger and recovery)
 	r := gin.Default()
+  r.Use(cors.Default()) //Warning: Allowing all origins disables cookies for clients. For credentialed requests, do not allow all origins.
 
 	// Define a simple GET endpoint
 	r.GET("/ping", func(c *gin.Context) {
